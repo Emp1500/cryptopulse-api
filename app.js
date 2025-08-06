3c3d04f6a97e65427ba9f7c4a1c0d987b9533106
Emp1500 <vedantwagh539@gmail.com>
2025-08-06 19:16:21 +0530

    commit: added dynamic EJS card rendering

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ app.set('views', path.join(__dirname, 'views')); // Always needed for EJS
 app.use(express.static(path.join(__dirname, 'public'))); // For serving CSS, images etc.
 
 // Routes
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+  try {
+    const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin,ethereum,binancecoin,solana,ripple,cardano&order=market_cap_desc&per_page=6&page=1&sparkline=false');
+    const data = await response.json();
+    res.render('index', {coins:data});  
+  } catch (error) {
+    console.log('Error fetching data:', error);
+    return res.status(500).send('Internal Server Error');
+  }
   res.render('index');
 });
 
