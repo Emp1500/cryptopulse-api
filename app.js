f8f6f8008a5e808da2f60af0b30865214f157ce9
Emp1500 <vedantwagh539@gmail.com>
2025-09-08 11:36:30 +0530

    Changed footer

--- app.js
+++ app.js
@@ -1,29 +1,38 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const axios = require('axios'); // Kept in case you add more API calls later
 
 // Middleware
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views')); // Always needed for EJS
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public'))); // For serving CSS, images etc.
 
 // Routes
 app.get('/', async (req, res) => {
   try {
-    const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin,ethereum,binancecoin,solana,ripple,cardano&order=market_cap_desc&per_page=6&page=1&sparkline=false');
-    const data = await response.json();
-    return res.render('index', { coins: data });
+    // Fetching the top 10 coins dynamically, as you originally planned.
+    const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets', {
+      params: {
+        vs_currency: 'usd',
+        order: 'market_cap_desc',
+        per_page: 12,
+        page: 1,
+        sparkline: false,
+        price_change_percentage: '24h'
+      },
+    });
+    const coins = response.data || [];
+    return res.render('index', { coins });
   } catch (error) {
-    console.log('Error fetching data:', error);
+    console.error('Error fetching data for homepage:', error.message);
     return res.status(500).send('Internal Server Error');
   }
 });
 
-
-app.get("/partials/graphs", (req, res) => {
-  res.render("graphs"); 
+app.get("/graphs", (req, res) => {
+  res.render("partials/graphs");
 });
-
 
 app.get('/home', (req, res) => {
   res.redirect('/');
@@ -32,7 +41,6 @@ app.get('/home', (req, res) => {
 app.get('/news', (req, res) => {
   res.redirect('https://coinmarketcap.com/headlines/news/');
 });
-
 
 app.get('/about', (req, res) => {
   res.render('partials/about');
--- public/js/widgets.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  var d = document, s = d.createElement('script');
-  s.src = 'https://widgets.coingecko.com/coingecko-coin-market-ticker-widget.js';
-  s.async = true;
-  d.head.appendChild(s);
-})();
-
-//used for CoinGecko widgets
